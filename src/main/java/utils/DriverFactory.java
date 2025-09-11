a6682d3bdc0fa4d9cac8985c151e0fdbed8a7c5e
OlegMuralev <92426862+OlegMuralev@users.noreply.github.com>
2025-09-11 17:31:40 +0500

    Driver Factory init

---
+++ src/main/java/utils/DriverFactory.java
@@ -0,0 +1,30 @@
+package utils;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DriverFactory {
+    private static AppiumDriver driver;
+
+    public static AppiumDriver getDriver() {
+        if (driver == null) {
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("platformName", "Android");
+            caps.setCapability("appium:automationName", "uiautomator2");
+            caps.setCapability("appium:deviceName", "emulator-5554");
+            caps.setCapability("appium:appPackage", "com.wdiodemoapp");
+            caps.setCapability("appium:appActivity", "com.wdiodemoapp.MainActivity");
+
+            try {
+                driver = new AndroidDriver(new URL("http://127.0.0.1:4725"), caps);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return driver;
+    }
+}
--- src/test/java/LaunchApp.java
+++ src/test/java/LaunchApp.java
@@ -1,28 +1,16 @@
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import utils.DriverFactory;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
 public class LaunchApp {
     public static void main(String[] args) throws MalformedURLException {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platformName", "Android");
-        caps.setCapability("appium:automationName", "uiautomator2");
-        caps.setCapability("appium:deviceName", "emulator-5554");
-        caps.setCapability("appium:appPackage", "com.wdiodemoapp");
-        caps.setCapability("appium:appActivity", "com.wdiodemoapp.MainActivity");
+       AppiumDriver driver = DriverFactory.getDriver();
 
-        AppiumDriver driver =
-                new AndroidDriver(new URL("http://127.0.0.1:4725"), caps);
-
-        driver.manage().timeouts().implicitlyWait(30L, TimeUnit.SECONDS);
-
-        WebElement loginLabel = driver.findElement(By.id("Login"));
+        WebElement loginLabel = driver.findElement(AppiumBy.accessibilityId("Login"));
+        loginLabel.click();
 
     }
 }
